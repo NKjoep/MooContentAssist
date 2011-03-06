aeffa8d024fb2dfcbea437e105f460ebe47c8963
Andrea Dessì <nkjoep@gmail.com>
2011-03-06 11:18:28 +0100

    sorting out v080

---
+++ demo/README.md
@@ -0,0 +1,3 @@
+This directory contains examples and demos.
+
+Please have a look at the index.html in the root of this project.
---
+++ demo/v0704/words.js
@@ -0,0 +1,45 @@
+{
+	"$content": {
+		"Immagine": ["getImagePath(\"\")", "text"],
+		"CorpoTesto": ["text", "textBeforeImage(0)", "textAfterImage(0)", "textByRange(0,0)"],
+		"Abstract": ["text"],
+		"Allegati": ["size()"],
+		"categories": [null],
+		"DataInizio": ["shortDate", "fullDate", "mediumDate", "getFormattedDate(\"\")"],
+		"DataFine": ["shortDate", "fullDate", "mediumDate", "getFormattedDate(\"\")"],
+		"SitoWeb": ["destination", "text"]
+	},
+	"$18n": ["getLabel(\"\")"],
+	"a": ["a1111", "a2222", "a33333"],
+	"albatros": ["quack"],
+	"albatro": [null],
+	"albeggiare": ["test1", "test2", "test3"],
+	"alberare": ["prop", "prop2", "prop3", "prop4", "prop5"],
+	"alberato": ["a", "b", "c", "d", "e", "f"],
+	"alberatura": ["ok"],
+	"b": ["bbb111", "bbbbb2222", "word-b2"],
+	"babbalotto": ["ouch"],
+	"babbeo": ["property"],
+	"babbino": ["getSomething", "getOther()", "isTrue()"],
+	"babbo": ["true"],
+	"babbuccia": [null],
+	"babbuino": [null],
+	"c": [null],
+	"contentAssist": ["help", "editor", "start"],
+	"MooTools": {
+		"Core": {
+			"$chk()": [null],
+			"$clear": [null],
+			"$defined": [null],
+			"$arguments": [null],
+			"$empty": [null],
+			"Class": ["implement()"],
+			"Native": {
+				"Array": ["each()", "every()", "filter()", "clean()"],
+				"String": ["test()", "contains()", "trim()"]
+			},
+			"Element": ["get()", "set()", "addClass()", "removeClass()"]
+		}
+	}					
+}
+}
